3b88b813b9f088ade2aa2ba5e1fde8aed2c7e2d6
Hillel Spivakov <hillel.spivakov@cyberark.com>
2021-04-04 15:58:35 +0300

    added navigation and react native elements library

---
+++ screens/AboutScreen.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import { Button } from 'react-native-elements';
+
+s
+export default function AboutScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go To Home"
+                onPress={() => navigation.navigate('Home')}
+            />
+        </View>
+    );
+}
---
+++ screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import { Button } from 'react-native-elements';
+
+
+
+export default function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go To About"
+                onPress={() => navigation.navigate('About')}
+
+            />
+        </View>
+    );
+}
