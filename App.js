729559b8e8df059ba4ae2f8e6aef065a29af70a1
Hillel Spivakov <hillel.spivakov@cyberark.com>
2021-04-05 13:02:55 +0300

    choose user type screen added

--- App.js
+++ App.js
@@ -5,20 +5,34 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
+import ChooseUserTypeScreen from './screens/ChooseUserTypeScreen';
+import { useFonts, Rubik_400Regular } from '@expo-google-fonts/rubik';
+import JobliLoader from './components/JobliLoader';
+import { StyleSheet } from 'react-native';
 
 
 
 const Stack = createStackNavigator();
 
 function App() {
-  return (
+  let [fontsLoaded] = useFonts({
+    Rubik_400Regular
+  });
+  if (!fontsLoaded) {
+    return <JobliLoader />
+  } else return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="ChooseUserTypeScreen">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen options={{ headerShown: false }} name="ChooseUserTypeScreen" component={ChooseUserTypeScreen} />
       </Stack.Navigator>
-    </NavigationContainer>
-  );
+    </NavigationContainer>);
+
 }
 
 export default App;
+
+const styles=StyleSheet.create({
+  
+})
---
+++ screens/ChooseUserTypeScreen.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+import { Button } from 'react-native-elements';
+import { JOBLI_FONT, PRIMARY_BTN, SECONDARY_BTN } from '../assets/theme';
+
+export default function ChooseUserTypeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+            <Image style={styles.imageStyle} resizeMode="contain" source={require('./../assets/images/JobliLogo.png')} />
+            <View style={styles.contentContainer}>
+                <Text style={[styles.textStyle, JOBLI_FONT]}>מה ברצונך לעשות?</Text>
+                <Button
+                    buttonStyle={[styles.choosetypeBtn, styles.seekingForWorkBtnStyle, PRIMARY_BTN.BTN_STYLE]}
+                    title="לחפש עבודה"
+                    titleStyle={PRIMARY_BTN.TITLE_STYLE}
+                    onPress={() => navigation.navigate('Home')}
+                />
+                <Button
+                    buttonStyle={[styles.choosetypeBtn, styles.seekingForEmployeesBtnStyle, SECONDARY_BTN.BTN_STYLE]}
+                    title="לחפש עובדים"
+                    titleStyle={SECONDARY_BTN.TITLE_STYLE}
+                    onPress={() => navigation.navigate('Home')}
+                />
+            </View>
+
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    imageStyle: {
+        width: 162,
+        height: 74,
+        marginTop: 100
+    },
+    textStyle: {
+        fontSize: 22,
+        fontFamily: 'Rubik',
+        color: '#000000',
+        marginBottom: 20,
+    },
+    choosetypeBtn: {
+        margin: 15
+    },
+    contentContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    seekingForWorkBtnStyle: {
+        width: 251,
+    },
+    seekingForEmployeesBtnStyle: {
+        width: 165,
+    }
+})
