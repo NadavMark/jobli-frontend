4bf2d36dc8d95f0eeb1d72f55181e5ff382e0919
cyberavisror <78016695+cyberavisror@users.noreply.github.com>
2021-04-05 14:49:51 +0300

    Create profile seeker (#1)
    * add create profile page
    
    * change component name to CreateProfileSeeker
    
    * navigate to SkillsQuestions page

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Theme from './theme';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
 import SkillsQuestionsScreen from './screens/SkillsQuestionsScreen'
 import SkillsSummary from './components/skills-wizard/skills-summary';
+import CreateProfileSeeker, { CreateProfileSeekerScreenName, CreateProfileSeekerScreenOptions } from './screens/create-profile/CreateProfileSeeker';
 
 const Stack = createStackNavigator();
 
+const navTheme = {
+  dark: false,
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: Theme.c1,
+    colors: Theme.textColor,
+    text: Theme.textColor,
+    border: Theme.c1
+  },
+};
+
 function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={navTheme}>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name={CreateProfileSeekerScreenName} options={CreateProfileSeekerScreenOptions} component={CreateProfileSeeker} />
         <Stack.Screen name="SkillsQuestions" component={SkillsQuestionsScreen} />
         <Stack.Screen name="SkillsSummary" component={SkillsSummary} />
       </Stack.Navigator>
