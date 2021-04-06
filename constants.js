68a0781dfed6675ab8fc00a3c9fef01e3fcd978b
Nadav Avisror <nadav.avisror@cyberark.com>
2021-04-06 20:51:22 +0300

    update

--- constants.js
+++ constants.js
@@ -4,7 +4,7 @@ export const EMPLOYERS = `${BASE_URL}/api/employers`;
 export const EMPLOYER = (id) => `${BASE_URL}/api/employers/${id}`;
 export const EMPLOYER_JOB_URL = (id) => `${BASE_URL}/api/employers/${id}/job`;
 export const SEEKER_PROFILE_ANSWERS_URL = (id) => `${BASE_URL}/api/seeker/${id}/answers`;
-export const SEEKER_LANGUAGES_URL = `${BASE_URL}/api/seeker/${id}/languages`;
+export const SEEKER_LANGUAGES_URL = (id) => `${BASE_URL}/api/seeker/${id}/languages`;
 
 // export const BASE_URL = 'https://b1f38ixvz0.execute-api.us-east-1.amazonaws.com/prod';
 // export const SEEKER_PROFILE_URL = (id) => `${BASE_URL}/api/seeker/profile`;
