3d4d09901c424214eec8af1f80f3098fd28486b6
yedri101 <64136147+yedri101@users.noreply.github.com>
2021-10-18 10:38:23 +0300

    Add start look for a job btn

--- screens/SummaryScreen.js
+++ screens/SummaryScreen.js
@@ -118,7 +118,7 @@ export default function SummaryScreen({ navigation }) {
         <Button
           title="התחל לחפש עבודה"
           onPress={() => {
-            navigateUserByType(userType);
+            navigation.navigate("JobsList");
           }}
         />
       )}
