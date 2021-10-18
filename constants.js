997bdbcb3887e02951ce430976b3bcd4a28af4aa
yedri101 <64136147+yedri101@users.noreply.github.com>
2021-10-18 08:50:47 +0300

    add user summary

--- constants.js
+++ constants.js
@@ -1,5 +1,7 @@
-export const BASE_URL = 'https://kwqwc5uojf.execute-api.eu-west-1.amazonaws.com/prod';
+export const BASE_URL = "https://kwqwc5uojf.execute-api.eu-west-1.amazonaws.com/prod";
 export const SEEKER_PROFILE_URL = (id) => `${BASE_URL}/api/seekers/${id}/profile`;
+export const SEEKER_SUMMARY = () => `${BASE_URL}/api/seekers/summary`;
+
 export const EMPLOYERS = `${BASE_URL}/api/employers`;
 export const EMPLOYER = (id) => `${BASE_URL}/api/employers/${id}`;
 export const EMPLOYER_JOB_URL = (id) => `${BASE_URL}/api/employers/${id}/job`;
